c8446de0c512a318c186508140de2bfa0edbbc1e
mateus degobi <kim3dis@gmail.com>
2023-06-28 23:42:44 -0300

    feat: add chords and config interval changes

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,18 @@
 import { useState, useEffect, useCallback } from "preact/hooks";
 import "./app.css";
 
+import C_chords from "./assets/C.png";
+import Dm_chords from "./assets/Dm.png";
+import F7m_chords from "./assets/F7m.png";
+
+const chords = [C_chords, Dm_chords, F7m_chords];
+
 export function App() {
   const [bpm, setBpm] = useState(60);
   const [isPlaying, setIsPlaying] = useState(false);
+  const [activeIndex, setActiveIndex] = useState(-1);
   const [audioContext, setAudioContext] = useState<AudioContext>();
+  const [bepIntervalo, setBepIntervalo] = useState<number>(3);
 
   const playSound = useCallback(() => {
     if (audioContext) {
@@ -20,10 +28,13 @@ export function App() {
     }
   }, [audioContext]);
 
-  const handleBpmChange = useCallback((e) => {
-    setBpm(Number(e.target.value));
-    playSound();
-  }, [playSound]);
+  const handleBpmChange = useCallback(
+    (e) => {
+      setBpm(Number(e.target.value));
+      playSound();
+    },
+    [playSound]
+  );
 
   const handleStartStop = useCallback(() => {
     setIsPlaying((prevIsPlaying) => !prevIsPlaying);
@@ -43,21 +54,52 @@ export function App() {
 
     if (isPlaying && audioContext) {
       const interval = (60 / bpm) * 1000;
-      intervalId = setInterval(playSound, interval);
+      intervalId = setInterval(() => {
+        playSound();
+        setActiveIndex((curr) => {
+          if (curr === chords.length * bepIntervalo) {
+            return 0;
+          }
+
+          return curr + 1;
+        });
+      }, interval);
     }
 
     return () => {
       clearInterval(intervalId);
     };
-  }, [bpm, isPlaying, audioContext, playSound]);
+  }, [bpm, isPlaying, audioContext, playSound, bepIntervalo]);
 
   return (
     <>
       <div>
         <h1>Metronome</h1>
         <div>
+          {chords.map((chord, index) => {
+            return (
+              <img
+                key={index}
+                src={chord}
+                className={`imageChords ${
+                  index * bepIntervalo === activeIndex && "activeImageChord"
+                }`}
+              />
+            );
+          })}
+        </div>
+
+        <div>
           <label>BPM:</label>
           <input type="number" value={bpm} onChange={handleBpmChange} />
+        </div>
+        <div>
+          <label>Intervalo de acordes por bep:</label>
+          <input
+            type="number"
+            value={bepIntervalo}
+            onChange={({ target }) => setBepIntervalo(target.value)}
+          />
         </div>
         <button onClick={handleStartStop}>
           {isPlaying ? "Stop" : "Start"}
